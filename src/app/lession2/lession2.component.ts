3337dc597af9ecbf496a6d0091df739a9f9e40aa
Pulppy Nguyễn <thocon7278@gmail.com>
2018-10-17 13:35:39 +0700

    Create Lession 2
    Change my-server => lession1
    Create Lession 2

---
+++ src/app/lession2/lession2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lession2',
+  templateUrl: './lession2.component.html',
+  styleUrls: ['./lession2.component.css']
+})
+export class Lession2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
